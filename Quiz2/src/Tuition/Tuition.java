61fbf3a9757670f99c129bf04f611ffa40663249
Jeffrey Attakorah <Jattak@udel.edu>
2016-09-27 02:26:38 -0400

    Update Tuition.java

--- Quiz2/src/Tuition/Tuition.java
+++ Quiz2/src/Tuition/Tuition.java
@@ -1,5 +1,28 @@
 package Tuition;
 
+import java.util.Scanner;
+
 public class Tuition {
 
+	public class TuitionCalculator {
+		private int iCost;
+		private int iIncrease;
+
+		Scanner input = new Scanner(System.in);
+		{
+			System.out.println("How much is your original tuition rate? ");
+			iCost = (int) input.nextDouble();
+			System.out.println("How much does your tuition plan to increase percentage wise? ");
+			iIncrease = (int) input.nextDouble();
+		}
+
+		public int iTuitionCalc(int iCost, int iIncrease) {
+			int x;
+			for (x = 0, x > 4, ++x); {
+				int payment = iCost + (iCost * (iIncrease / 100));
+				return payment;}
+		}
+
+	}
 }
+
